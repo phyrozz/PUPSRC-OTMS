6abbac444e12026d9f53844068b065b1881d3082
gabbyyy01 <gabrielangelooliver@gmail.com>
2023-05-19 15:58:17 +0800

    updated links to office homepage

--- student/academic/shifting.php
+++ student/academic/shifting.php
@@ -63,7 +63,7 @@
         <div class="container-fluid academicbanner header" style="height:250px">
         <nav class="breadcrumb-nav breadcrumb-container" aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a href="../home.php">Home</a></li>
                     <li class="breadcrumb-item"><a href="../academic.php">Academic Office</a></li>
                     <li class="breadcrumb-item active" aria-current="page">Shifting</li>
                 </ol>
