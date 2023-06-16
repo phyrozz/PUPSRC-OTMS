1cc09ef15d2529af0d3691dfefcfaeb4a8066833
Sievoncoldblade <jlcsievon@gmail.com>
2023-06-16 19:17:08 +0800

    registrar office, may mga finix na bug lol

---
+++ client/registrar/submit_request.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include 'conn.php';
+$user_id = 28;
+$status_id = 1;
+$office_id = 1;
+$date = $_SESSION['date'];
+$services_id = $_SESSION['req_student_service'];
+
+unset($_SESSION['date']);
+unset($_SESSION['req_student_service']);
+
+$query = "INSERT INTO reg_transaction (office_id, services_id, schedule, status_id, user_id) VALUES (?, ?, ?, ?, ?)";
+$stmt = mysqli_prepare($connect, $query);
+mysqli_stmt_bind_param($stmt, "iisii", $office_id, $services_id, $date, $status_id, $user_id);
+$result = mysqli_stmt_execute($stmt);
+
+if ($result) {
+    // Query executed successfully
+    echo "<script>alert(Record inserted successfully.);</script>";
+} else {
+    // Error executing the query
+    echo "<script>alert('Error: " . mysqli_error($connect) . "')</script>";
+}
+
+echo "<script>window.location.href = 'create_request.php';</script>";
+exit;
+?>
