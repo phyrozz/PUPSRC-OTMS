5079c3c7abd9d7e09be995173b1be89d0ddd7a2f
arkohn1 <johnmarkgarapan2@gmail.com>
2023-06-18 12:49:18 +0800

    updated accounting Files final v2.1 latest

--- student/accounting/payment2.php
+++ student/accounting/payment2.php
@@ -1,6 +1,6 @@
 <!-- INSERT PHP SECTION -->
 <?php
-session_start();
+$office_name = "Accounting Office";
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -47,80 +47,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/payment2.css">
     <script src="https://kit.fontawesome.com/fe96d845ef.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/style.css">
     <script src="../../node_modules/jquery/dist/jquery.min.js"></script>
     <script src="../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-    <!-- Start of navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-maroon p-3">
-        <div class="container-fluid">
-            <img class="p-2" src="images/puplogo.png" alt="PUP Logo" width="40">
-            <a class="navbar-brand" href="#"><strong>PUPSRC-OTMS</strong></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto order-2 order-lg-1">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="officeServicesDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Accounting Services Office
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="officeServicesDropdown">
-                            <li><a class="dropdown-item" href="#">Registration</a></li>
-                            <li><a class="dropdown-item" href="../guidance.php">Guidance</a></li>
-                            <li><a class="dropdown-item" href="#">Academic</a></li>
-                            <li><a class="dropdown-item" href="index.php">Accounting</a></li>
-                            <li><a class="dropdown-item" href="#">Administration</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="officeServicesDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Payments
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="officeServicesDropdown">
-                            <li><a class="dropdown-item" href="offsetting1.php">Offsetting</a></li>
-                            <li><a class="dropdown-item" href="transactions.php">Transaction History</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <ul class="navbar-nav order-3 order-lg-3 w-50 gap-3">
-                    <div class="d-flex navbar-nav justify-content-center me-auto order-2 order-lg-1 w-100">
-                        <form class="d-flex w-100">
-                            <input class="form-control me-2" type="search" placeholder="Search for services..."
-                                aria-label="Search">
-                            <button class="btn btn-warning" type="submit"><strong>Search</strong></button>
-                        </form>
-                    </div>
-                    <li class="nav-item dropdown order-1 order-lg-2">
-                        <a class="nav-link dropdown-toggle" href="#" id="userProfileDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="fa fa-user-circle me-1"></i>
-                            Juan Dela Cruz
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userProfileDropdown">
-                            <li><a class="dropdown-item" href="#">Account Settings</a></li>
-                            <li><a class="dropdown-item" href="#">Log Out</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- End of navbar -->
-
+<?php
+    include '../navbar.php';
+    include '../../breadcrumb.php';
+    ?>
     <div class="container-fluid p-4">
-        <nav class="breadcrumb-nav" aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="home.php">Home</a></li>
-                <li class="breadcrumb-item"><a href="index.php">Accounting Services Office</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Payments</li>
-            </ol>
-        </nav>
+        <?php
+        $breadcrumbItems = [
+            ['text' => 'Accounting Office', 'url' => '../accounting.php', 'active' => false],
+            ['text' => 'Payments', 'active' => true],
+        ];
+
+        echo generateBreadcrumb($breadcrumbItems, true);
+        ?>
     </div>
+
+  
     <div class="container-fluid text-center p-4">
         <!--Start of content-->
         <div class="container-fluid-form">
