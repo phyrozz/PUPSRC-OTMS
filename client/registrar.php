6a6ceae6da6aa1511d5cca324ef297b9548e1f2b
Sievoncoldblade <jlcsievon@gmail.com>
2023-06-24 01:02:43 +0800

    removed also in registrar.php

--- client/registrar.php
+++ client/registrar.php
@@ -14,14 +14,16 @@
 	<script src="../node_modules/jquery/dist/jquery.min.js"></script>
 	<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script> -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/x-icon" href="../assets/favicon.ico">
-    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../style.css">
-    <script src="https://kit.fontawesome.com/fe96d845ef.js" crossorigin="anonymous"></script>
-    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
-    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+		rel="stylesheet">
+	<link rel="icon" type="image/x-icon" href="../assets/favicon.ico">
+	<link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../style.css">
+	<script src="https://kit.fontawesome.com/fe96d845ef.js" crossorigin="anonymous"></script>
+	<script src="../node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
@@ -56,34 +58,29 @@
 				<h2>Your Registrar Transactions</h2>
 				<p>Status of current request with the registrar office</p>
 			</a>
-			<a href="./registrar/registrar_history.php"
-				class="btn btn-primary d-block text-decoration-none bg-maroon text-light p-4 rounded-0">
-				<h2>Registrar Transaction History</h2>
-				<p>Check previous completed request from registrar office</p>
-			</a>
+
+			<div class="push"></div>
 		</div>
-		<div class="push"></div>
-	</div>
-	<footer
-		class="footer container-fluid w-100 text-md-left text-center d-md-flex align-items-center justify-content-center bg-light flex-nowrap">
-		<div>
-			<small>PUP Santa Rosa - Online Transaction Management System Beta 0.1.0</small>
-		</div>
-		<div>
-			<small><a href="https://www.pup.edu.ph/terms/" target="_blank" class="btn btn-link">Terms of Use</a>|</small>
-			<small><a href="https://www.pup.edu.ph/privacy/" target="_blank" class="btn btn-link">Privacy
-					Statement</a></small>
-		</div>
-	</footer>
-	<script>
-	$(document).ready(function() {
-		$('.dropdown-submenu a.dropdown-toggle').on("click", function(e) {
-			$(this).next('ul').toggle();
-			e.stopPropagation();
-			e.preventDefault();
+		<footer
+			class="footer container-fluid w-100 text-md-left text-center d-md-flex align-items-center justify-content-center bg-light flex-nowrap">
+			<div>
+				<small>PUP Santa Rosa - Online Transaction Management System Beta 0.1.0</small>
+			</div>
+			<div>
+				<small><a href="https://www.pup.edu.ph/terms/" target="_blank" class="btn btn-link">Terms of Use</a>|</small>
+				<small><a href="https://www.pup.edu.ph/privacy/" target="_blank" class="btn btn-link">Privacy
+						Statement</a></small>
+			</div>
+		</footer>
+		<script>
+		$(document).ready(function() {
+			$('.dropdown-submenu a.dropdown-toggle').on("click", function(e) {
+				$(this).next('ul').toggle();
+				e.stopPropagation();
+				e.preventDefault();
+			});
 		});
-	});
-	</script>
+		</script>
 </body>
 
 </html>
