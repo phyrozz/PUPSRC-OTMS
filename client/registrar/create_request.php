1ccb68b8eb859fe954dcaaacdf9ad03191aa0491
Sievoncoldblade <jlcsievon@gmail.com>
2023-06-24 01:06:18 +0800

    fixed navigation on registrar again

--- client/registrar/create_request.php
+++ client/registrar/create_request.php
@@ -103,7 +103,7 @@ if(isset($_POST["submit"])){
 		<div class="container-fluid p-4">
 			<nav class="breadcrumb-nav" aria-label="breadcrumb">
 				<ol class="breadcrumb">
-					<li class="breadcrumb-item"><a href="#">Home</a></li>
+					<li class="breadcrumb-item"><a href="../home.php">Home</a></li>
 					<li class="breadcrumb-item"><a href="../registrar.php">Registrar Office</a></li>
 					<li class="breadcrumb-item active" aria-current="page">Create Request</li>
 				</ol>
