46c0f3bcc980bacccf0a51769eb4e05b550aa159
rebb11 <joserebomanguerra@gmail.com>
2023-07-16 11:46:42 +0800

    removed offsettings in client side

--- client/accounting.php
+++ client/accounting.php
@@ -49,10 +49,6 @@
                 <h2>PAYMENTS</h2>
                 <p>Simplify your payments for campus documents</p>
             </a>
-            <a href="accounting/offsetting1.php" class="btn btn-primary d-block text-decoration-none bg-maroon text-light p-4 rounded-0">
-            <h2>OFFSETTING</h2>
-                <p>Balance your campus accounts</p>
-            </a>
             <a href="transactions.php" class="btn btn-primary d-block text-decoration-none bg-maroon text-light p-4 rounded-0">
             <h2>TRANSACTION HISTORY</h2>
                 <p>Access your past and current transactions</p>
