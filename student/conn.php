035f65faa69feb383953e1b0c26f77ac0823dfcc
Paulita <133938516+helomih@users.noreply.github.com>
2023-07-21 12:11:38 +0800

    updated survey

--- student/conn.php
+++ student/conn.php
@@ -1,12 +1,32 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "reg_db";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "otms_db";
 
-    $connect = new mysqli($servername, $username, $password, $database);
+// Create connection
+$connect = new mysqli($servername, $username, $password, $database);
 
-    if ($connect->connect_error) {
-        die("Connection failed: " . $connect->connect_error);
+// Check connection
+if ($connect->connect_error) {
+    die("Connection failed: " . $connect->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Escape user input to prevent SQL injection
+    $rating = $connect->real_escape_string($_POST["rating"]);
+    $suggestions = $connect->real_escape_string($_POST["suggestions"]);
+
+    $sql = "INSERT INTO acad_survey (rating, suggestions) VALUES ('$rating', '$suggestions')";
+
+    if ($connect->query($sql) === true) {
+        // Success! Redirect to "transactions.php"
+        header("Location: transactions.php");
+        exit; // Make sure to exit after the redirect
+    } else {
+        echo "Error: " . $sql . "<br>" . $connect->error;
     }
+}
+
+$connect->close();
 ?>
